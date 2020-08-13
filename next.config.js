43eba6ce442702e266ee0bb774a47c70875744ad
Ralph Simon <ralph.simon2012@icloud.com>
2020-08-13 21:22:31 +0200

    Follow tutorial steps

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const data = require('./utils/projectsData')
+
+module.exports = {
+	exportTrailSlash: true,
+	exportPathMap: async function () {
+		const { projects } = data
+
+		const paths = {
+			'/': { page: '/' },
+		}
+
+		projects.forEach((project) => {
+			paths[`/project/${project.slug}`] = {
+				page: '/project/[path]',
+				query: {
+					path: project.slug,
+				},
+			}
+		})
+
+		return paths
+	},
+}
